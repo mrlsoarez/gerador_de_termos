ec70de88d24609229ca496081a7501de3187e256
mrlsoarez <murilosoliveira202@gmail.com>
2026-04-01 08:16:59 -0300

    [update april month

--- MODULES/object_creator/create_object.py
+++ MODULES/object_creator/create_object.py
@@ -207,7 +207,7 @@ def capturar_info_planilha(localizacao_planilha, numero_especifico = False):
             if ("ATA" in localizacao_planilha):
                 gestor = "MURILO SOARES DE OLIVEIRA\nGESTOR DE ATA"
             else: 
-                gestor = "RONALDO DE SOUZA MARCILIO\nGESTOR DE CONTRATO"
+                gestor = "MURILO SOARES DE OLIVEIRA\nSETOR DE CONTRATOS"
         
             novo_termo = Termo(ordem, contrato, contratado, objeto, af, mensagem, gestor)
             novo_termo.setRelatorioInfo(liquidacao, valor, data_liquidacao)
