64b11fe33b89bf3863098a7a5c8aab8446cdc23f
mrlsoarez <murilosoliveira202@gmail.com>
2026-05-28 12:00:07 -0300

    []

--- ENV/environment.py
+++ ENV/environment.py
@@ -4,7 +4,7 @@ ROOT = r"C:\Users\Usuario\Documents\MRL"
 TXT_PROTOCOLO = r"C:\Program Files (x86)\numero_protocolo\protocolo.txt"
 
 def pegar_endereco_base():
-    return rf"{ROOT}\1. ANÁLISE DE PAGAMENTOS\ANÁLISE MENSAL\2026\4. ABRIL"
+    return rf"{ROOT}\1. ANÁLISE DE PAGAMENTOS\ANÁLISE MENSAL\2026\5. MAIO"
   
 def pegar_planilha_termo(arquivo):
     return rf"{ROOT}\1. ANÁLISE DE PAGAMENTOS\INFO\{arquivo}"
